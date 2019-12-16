753951e8da211bf273f64b8ce1095b352ca475d4
reemharbi <reemharbi.3@gmail.com>
2019-12-17 02:15:24 +0300

    Moved Suggestion Button up and Removed SuggestionButton Component

--- src/Components/WelcomeScreen/Welcome.js
+++ src/Components/WelcomeScreen/Welcome.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "../../Assets/Images/wain-nakl.png";
 import "./Welcome.css";
-import Suggestion from "./SuggestionButton";
+export default class Welcome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { active: false };
+  }
 
-export default function Welcome() {
-  
-  return (
-    // <div className="main animated slideOutUp delay-5s">
-    <div className="main">
-      <div className="container">
-        <img className="logo" src={logo} alt="logo"></img>
-        <p className="title">وين ناكل؟</p>
-        <Suggestion />
+  handleSuggestionClick = e => {
+    this.setState({ active: true });
+    e.stopPropagation();
+    console.log("handling Suggestion Click!");
+  };
+
+  render() {
+    const animation = this.state.active ? "animated slideOutUp" : "";
+    return (
+      // <div className="main animated slideOutUp delay-5s">
+      <div className={`main ${animation}`}>
+        <div className="container">
+          <img className="logo" src={logo} alt="logo"></img>
+          <p className="title">وين ناكل؟</p>
+        <button className='btn' onClick={this.handleSuggestionClick}>اقترح</button>
+         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
