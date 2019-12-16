6b10de225a16ec051d4b25428aade7b273139827
reemharbi <reemharbi.3@gmail.com>
2019-12-17 01:43:16 +0300

    Handle suggestion click

--- src/Components/WelcomeScreen/SuggestionButton.js
+++ src/Components/WelcomeScreen/SuggestionButton.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import './SuggestionButton.css'
+import React, { Component } from "react";
+import "./SuggestionButton.css";
 
-export default function SuggestionButton() {
+export default class SuggestionButton extends Component {
+    handleClick = e => {
+        e.stopPropagation();
+        console.log("handling Suggestion Click!");
+      };
+
+  render() {
     return (
-        <div>
-            <button className='btn'>اقترح</button>
-        </div>
-    )
+      <div>
+        <button className="btn" onClick={this.handleClick}>اقترح</button>
+      </div>
+    );
+  }
 }
